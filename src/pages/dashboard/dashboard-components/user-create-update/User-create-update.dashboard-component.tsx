d20de58fead7b4cda8c38664c62196ebf1195f9c
Mohamed Siraj <siraj@oganro.com>
2023-11-05 16:33:47 +0530

    ui completed

--- src/pages/dashboard/dashboard-components/user-create-update/User-create-update.dashboard-component.tsx
+++ src/pages/dashboard/dashboard-components/user-create-update/User-create-update.dashboard-component.tsx
@@ -4,22 +4,28 @@ const UserCreateUpdateDashboardComponent: React.FunctionComponent = () => {
 
     return (<>
         <div
-            className="m-10 block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-            <h5
-                className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                User Dashboard
-            </h5>
-            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                Some quick example text to build on the card title and make up the bulk
-                of the card's content.
-            </p>
-            <button
-                type="button"
-                className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                data-te-ripple-init
-                data-te-ripple-color="light">
-                Button
-            </button>
+            className="m-10 block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)">
+            <form>
+                <div className="grid grid-cols-1 ">
+                    <div className="mt-4 mb-3">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Username
+                        </label>
+                        <input className={`border-rose-600 shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} type="text" placeholder="Enter your name" />
+                    </div>
+                    <div className="mt-4 mb-3">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Job
+                        </label>
+                        <input className={`border-rose-600 shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} type="text" placeholder="Enter your job" />
+                    </div>
+                    <div className="mt-3 mb-3 text-end">
+                        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-[12rem]">
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </>);
 };
--- src/pages/dashboard/dashboard-components/user-list/User-list.dashboard-component.tsx
+++ src/pages/dashboard/dashboard-components/user-list/User-list.dashboard-component.tsx
@@ -5,9 +5,9 @@ const UserListDashboardComponent: React.FunctionComponent = () => {
 
     return (<>
         <div
-            className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 m-10 ">
+            className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] m-10 ">
             <h5
-                className="mb-4 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                className="mb-4 text-xl font-medium leading-tight text-neutral-80">
                 Mohamed Siraj
             </h5>
             <div className="flex space-x-6">
