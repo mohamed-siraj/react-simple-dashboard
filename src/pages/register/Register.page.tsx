cae9518c2a7572af6030419fcc68ac78dfff307b
Mohamed Siraj <siraj@oganro.com>
2023-11-05 14:33:33 +0530

    created register page

--- src/pages/register/Register.page.tsx
+++ src/pages/register/Register.page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-class LoginPage extends React.Component {
-    
+class RegisterPage extends React.Component {
+
     render(): React.ReactNode {
         return (<>
             <div className="flex flex-row justify-center py-64">
-                <div className="bg-white w-96 h-[29rem] rounded-2xl shadow-2xl">
+                <div className="bg-white w-96 h-[32rem] rounded-2xl shadow-2xl">
                     <div className="mt-10 text-center text-2xl font-bold">
-                        <h1>Login</h1>
+                        <h1>Registration</h1>
                     </div>
                     <div className="flex flex-row justify-center">
                         <div className="flex flex-col">
@@ -24,13 +24,19 @@ class LoginPage extends React.Component {
                                 <input className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} type="password" placeholder="Password" />
                             </div>
                             <div className="mt-3 mb-3 w-80">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                    Password
+                                </label>
+                                <input className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`} type="password" placeholder="Password" />
+                            </div>
+                            <div className="mt-3 mb-3 w-80">
                                 <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full">
                                     Login
                                 </button>
                             </div>
                             <div className="mt-3 mb-3 w-80">
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-                                    Register
+                                <button className="bg-black text-white font-bold py-2 px-4 rounded w-full">
+                                    Back
                                 </button>
                             </div>
                         </div>
@@ -38,8 +44,9 @@ class LoginPage extends React.Component {
 
                 </div>
             </div>
-        </>)
+        </>);
     }
+
 };
 
-export default LoginPage;
+export default RegisterPage;
--- src/routers/without-auth-route.tsx
+++ src/routers/without-auth-route.tsx
@@ -1,10 +1,15 @@
 import { RouteObject } from "react-router-dom";
 import LoginPage from "../pages/login/Login.page";
+import RegisterPage from "../pages/register/Register.page";
 
 const WITHOUT_AUTH_ROUTE : RouteObject[] = [
     {
         path: '/login',
         element: <LoginPage/>,
+    },
+    {
+        path: '/register',
+        element: <RegisterPage/>,
     }
 ];
 
